8d0247fc28fcb0da46214acfe78ebb3b2fcbf062
LyashenkoAndron <lyashenko.andron@mail.ru>
2020-11-24 17:18:40 +0300

    Создан Unity проект

---
+++ Assets/scripts/bullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    public int Speed;
+    Vector3 LastPos;
+    void Start()
+    {
+        LastPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Speed * Time.deltaTime);
+        RaycastHit hit;
+        if (Physics.Linecast(LastPos, transform.position, out hit))
+        {
+            print(hit.transform.name);
+            Destroy(gameObject);
+        }
+        LastPos = transform.position;
+    }
+}
